f65e2019646b304dddb68f466bd4566440164e39
guozhengzhong <guozhengzhong@tal.com>
2021-08-14 20:37:01 +0800

    build: 添加项目代码校验功能

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
